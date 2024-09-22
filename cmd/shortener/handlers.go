b7faaf400ede945aaa8dffe4037e1429baf58422
DeMurk <62705843+demurk@users.noreply.github.com>
2024-09-22 19:29:14 +0300

    Created simple server

---
+++ cmd/shortener/handlers.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func postPage(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(res, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
+		return
+	}
+	fullURLBytes, err := io.ReadAll(req.Body)
+	if err != nil {
+		http.Error(res, "Couldn't read request body", http.StatusInternalServerError)
+		return
+	}
+	shortURLId := setFullURL(string(fullURLBytes))
+	res.Header().Set("content-type", "text/plain")
+	res.WriteHeader(http.StatusCreated)
+	res.Write([]byte("http://localhost:8080/" + shortURLId))
+}
+
+func idPage(res http.ResponseWriter, req *http.Request) {
+	shortURL := req.PathValue("id")
+	fullURL, err := getFullURL(shortURL)
+	if err != nil {
+		http.Error(res, "Url doesnt exists", http.StatusNotFound)
+		return
+	}
+	res.Header().Set("Location", "text/plain")
+	http.Redirect(res, req, fullURL, http.StatusTemporaryRedirect)
+}
