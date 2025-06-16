5a25ccf80e876bb89725f37e959c9aef7b5664f3
DeMurk <62705843+demurk@users.noreply.github.com>
2025-06-16 21:15:45 +0300

    Fix

--- cmd/shortener/config/config.go
+++ cmd/shortener/config/config.go
@@ -6,7 +6,7 @@ import (
 )
 
 var (
-	OriginURL = flag.String("a", "localhost:8080", "Origin server url")
+	OriginURL = flag.String("a", "http://localhost:8080", "Origin server url")
 	ResultURL = fmt.Sprintf("%s/%s", *OriginURL, *flag.String("b", "", "Base result server url"))
 )
 
