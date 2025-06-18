6d17391661bdcbd09cd5784b8da8650436fdc9ec
demurk <62705843+demurk@users.noreply.github.com>
2025-06-18 15:37:20 +0300

    Add zap logger middleware

--- cmd/shortener/server.go
+++ cmd/shortener/server.go
@@ -4,21 +4,31 @@ import (
 	"net/http"
 
 	"github.com/demurk/tinyurl/cmd/shortener/config"
-	"github.com/go-chi/chi/middleware"
+	"github.com/demurk/tinyurl/internal/app/middleware/logger"
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 )
 
 func main() {
 	config.Parse()
+
+	zapLogger, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+	defer zapLogger.Sync()
+
 	r := chi.NewRouter()
+
+	r.Use(logger.ZapLogger(zapLogger))
 	r.MethodNotAllowed(func(res http.ResponseWriter, r *http.Request) {
 		http.Error(res, "Invalid request method", http.StatusMethodNotAllowed)
 	})
-	r.Use(middleware.Logger)
+
 	r.Post("/", postPage)
 	r.Get("/{id}", getPage)
 
-	err := http.ListenAndServe(*config.OriginURL, r)
+	err = http.ListenAndServe(*config.OriginURL, r)
 	if err != nil {
 		panic(err)
 	}
