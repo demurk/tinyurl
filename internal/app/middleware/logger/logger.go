b5974f123f002a0e6f21572a7187195a5486ffa3
DeMurk <62705843+demurk@users.noreply.github.com>
2025-06-18 15:34:59 +0300

    Add zap logger middleware

---
+++ internal/app/middleware/logger/logger.go
@@ -0,0 +1,33 @@
+package logger
+
+import (
+	"bytes"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/middleware"
+	"go.uber.org/zap"
+)
+
+func ZapLogger(logger *zap.Logger) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			rw := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+			var responseBody bytes.Buffer
+			rw.Tee(&responseBody)
+
+			start := time.Now()
+			next.ServeHTTP(rw, r)
+
+			logger.Info("Request info",
+				zap.String("URI", r.RequestURI),
+				zap.String("method", r.Method),
+				zap.Duration("duration_seconds", time.Since(start)),
+			)
+			logger.Info("Response info",
+				zap.Int("status", rw.Status()),
+				zap.Int("response_size_bytes", rw.BytesWritten()),
+			)
+		})
+	}
+}
